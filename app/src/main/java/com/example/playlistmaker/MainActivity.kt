f2d110aaa0584d9043f912036e6e4ae98dcc7548
Florid <Florid.95@mail.ru>
2026-01-14 23:13:35 +0500

    Спринт 8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                val displaySearch = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(displaySearch)
+            }
+        }
+        buttonSearch.setOnClickListener (searchClickListener)
+
+        val buttonMedicalLibrary = findViewById<Button>(R.id.medical_library)
+        buttonMedicalLibrary.setOnClickListener {
+            val displayMedicalLibrary = Intent(this@MainActivity, MedicalLibraryActivity::class.java)
+            startActivity(displayMedicalLibrary)
+        }
 
 
-            insets
+        val buttonSettings = findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener {
+            val displaySettings = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(displaySettings)
         }
     }
 }

37821c8243939ccffcdf0171aa857e13477c830f
Florid <Florid.95@mail.ru>
2026-02-11 19:58:49 +0500

    Спринт 9. Домашнее задание

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,12 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private var textSearch: String = TEXT_SEARCH_VALUE
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_SEARCH_KEY, textSearch)
+    }
+    companion object {
+        const val TEXT_SEARCH_KEY = "TEXT_SEARCH"
+        const val TEXT_SEARCH_VALUE = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +35,56 @@ class SearchActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val buttonBack = findViewById<MaterialToolbar>(R.id.toolbar_search)
+        buttonBack.setNavigationOnClickListener {
+            finish()
+        }
+        val inputTextSearch = findViewById<EditText>(R.id.input_text_search)
+        if (savedInstanceState != null) {
+            textSearch = savedInstanceState.getString(TEXT_SEARCH_KEY, TEXT_SEARCH_VALUE)
+            if (textSearch.isNotEmpty()){
+                inputTextSearch.setText(textSearch)
+            }
+        }
+        val buttonClearSearch = findViewById<ImageView>(R.id.button_clear_search)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        //val view = this.currentFocus
+        buttonClearSearch.setOnClickListener {
+            inputTextSearch.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+
+        }
+        val searchTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+                //
+            }
+            override fun onTextChanged(
+                s: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+                buttonClearSearch.visibility = buttonClearSearchVisibility(s)
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                textSearch = s.toString()
+            }
+        }
+        inputTextSearch.addTextChangedListener(searchTextWatcher)
+    }
+
+    private fun buttonClearSearchVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
+import androidx.core.net.toUri
 
 class SettingsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +29,30 @@ class SettingsActivity: AppCompatActivity() {
             finish()
         }
 
+        val buttonShareTheApp = findViewById<MaterialTextView>(R.id.share_the_app)
+        buttonShareTheApp.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/*"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_the_app_uri))
+            val chosenIntent = Intent.createChooser(intent, getString(R.string.share_the_app))
+            startActivity(chosenIntent)
+        }
+
+        val buttonWriteToSupport = findViewById<MaterialTextView>(R.id.write_to_support)
+        buttonWriteToSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = getString(R.string.write_to_support_uri).toUri()
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.write_to_support_subject))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.write_to_support_text))
+            startActivity(intent)
+        }
+
+        val buttonUserAgreement = findViewById<MaterialTextView>(R.id.user_agreement)
+        buttonUserAgreement.setOnClickListener {
+            val address = getString(R.string.user_agreement_uri).toUri()
+            val intent = Intent(Intent.ACTION_VIEW, address)
+            intent.addCategory(Intent.CATEGORY_BROWSABLE)
+            startActivity(intent)
+        }
     }
 }
